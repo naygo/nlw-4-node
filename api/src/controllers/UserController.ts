d629628126516934733a44293ea4a5fcdcf87e4b
Nayla Gomes <nayla.cgs@gmail.com>
2021-02-24 17:47:34 -0300

    Terceira aula

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
-
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 class UserController {
 
     async create(req: Request, res: Response) {
         const { name, email } = req.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -22,7 +21,7 @@ class UserController {
 
         await usersRepository.save(user);
 
-        return res.json(user);
+        return res.status(201).json(user);
     }
 };
 
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userControler = new UserController();
+const surveysControler = new SurveysController();
 
 router.post('/users', userControler.create);
+
+router.get('/surveys', surveysControler.show);
+router.post('/surveys', surveysControler.create);
 
 export { router };

31c6f4cb49e0474d9e2b80b8ce2117ec9da8c0c3
Nayla Gomes <nayla.cgs@gmail.com>
2021-02-25 14:40:34 -0300

    Quarta aula.

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,15 +1,25 @@
 import { Router } from 'express';
+
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userControler = new UserController();
 const surveysControler = new SurveysController();
+const sendMailController = new SendMailController()
 
+// Users
 router.post('/users', userControler.create);
 
+// Surveys
 router.get('/surveys', surveysControler.show);
 router.post('/surveys', surveysControler.create);
+
+// SurveysUsers
+router.post('/sendMail', sendMailController.execute);
+router.delete('/sendMail', sendMailController.delete);
+
 
 export { router };
